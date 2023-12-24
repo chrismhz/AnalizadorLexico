73213bbba4ce120a50273facc2ae353464ae4f30
chrismhz <martinez.christian.isic@gmail.com>
2023-12-24 13:16:37 -0600

    Se agrega el Proyecto

---
+++ src/codigo/Tokens.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Enum.java to edit this template
+ */
+package codigo;
+
+/**
+ *
+ * @author LENOVO
+ */
+public enum Tokens {
+    Linea,
+    Int,
+    If,
+    Else,
+    While,
+    Igual,
+    Suma,
+    Resta,
+    Multiplicacion,
+    Division,
+    Parentesis_a,
+    Parentesis_c,
+    Llave_a,
+    Llave_c,
+    Main,
+    P_coma,
+    Identificador,
+    Numero,
+    ERROR  
+}
